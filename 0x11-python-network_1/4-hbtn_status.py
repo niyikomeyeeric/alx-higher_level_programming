36e5a88c609a972a8c49c5ec5b340739e877ec76
niyikomeyeeric <niyikomeye@gmail.com>
2022-09-23 18:43:15 +0200

    fixing errors

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ import requests
 
 
 if __name__ == "__main__":
-    result_url = 'https://intranet.hbtn.io/status'
+    result_url = 'https://alx-intranet.hbtn.io/status'
     print("Body response:")
     print("\t- type: {}".format(type(requests.get(result_url).text)))
     print("\t- content: {}".format(requests.get(result_url).text))
