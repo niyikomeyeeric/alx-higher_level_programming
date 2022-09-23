a00f706fba44784b579e662cfa7998279f186710
niyikomeyeeric <niyikomeye@gmail.com>
2022-09-23 19:11:12 +0200

    fixing errors

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""that takes in a letter and sends a POST request to"""
+"""Takes in Github repo nd owner name to list
+10 commits (from the most recent to oldest)"""
 
 
-from requests import get, auth
-import sys
 if __name__ == "__main__":
-    try:
-        repo = sys.argv[1]
-        owner = sys.argv[2]
-        url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
-        r = get(url)
-        json_o = r.json()
-        for i in range(0, 10):
-            print("{}: {}".format(json_o[i].get('sha'), json_o[i].get('commit')
-                                  .get('author').get('name')))
-    except:
-        pass
+    import requests
+    import sys
+
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(sys.argv[2], sys.argv[1]))
+    if r.status_code >= 400:
+        print('None')
+    else:
+        for com in r.json()[:10]:
+            print("{}: {}".format(com.get('sha'),
+                                  com.get('commit').get('author').get('name')))
