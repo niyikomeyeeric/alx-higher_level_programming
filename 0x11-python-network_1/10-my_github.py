7324733f8f0f08ec614605ec8a1e383c17504e4a
niyikomeyeeric <niyikomeye@gmail.com>
2022-09-23 18:19:48 +0200

    0x11-python-network_1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""that takes in a letter and sends a POST request to"""
+"""Takes your GitHub credentials (username and password) and uses
+the GitHub API to display your id."""
+import requests
+from sys import argv
 
 
-def main():
-    import requests
-    from sys import argv
-    from requests.auth import HTTPBasicAuth
-
-    url = "https://api.github.com/user"
-    req = requests.get(url, auth=HTTPBasicAuth(argv[niyikomeyeeric], argv[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])).json()
-    print(req.get("id"))
 if __name__ == "__main__":
-    main()
+    url = "https://api.github.com/user"
+    usern = argv[1]
+    token = argv[2]
+    resp = requests.get(url, auth=(usern, token))
+    print("{}".format(resp.json().get("id")))
